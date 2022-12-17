c430e3783bbf5ce37f78d9d814b1d110bdf71fab
smithac-ob <alan@onebillion.org>
2022-12-17 10:36:42 +0000

    Initial Commit

---
+++ aucmp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  aucmp
+//
+//  Created by Alan Smith on 17/12/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
